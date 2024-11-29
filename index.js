721bfe872258a49d89646d11fb7170f80e2f4348
Iman Aboheydary <iman@customgento.com>
2024-11-29 10:50:34 +0100

    parent child

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const { app, BrowserWindow } = require('electron')
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
-        height: 600
+        height: 600,
     })
 
-    win.loadFile('index.html')
+    win.loadFile('index.html');
+
+    const child = new BrowserWindow({ parent: win });
+    child.loadURL('https://kreando.ch');
 }
 
 app.whenReady().then(() => {
-    createWindow()
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
 })
