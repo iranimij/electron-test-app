498281383f16bcd1997e430ce891192970868c40
Iman Aboheydary <iman@customgento.com>
2024-11-29 11:15:19 +0100

    shell module

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-const { app, BrowserWindow, shell } = require('electron')
+const { app, BrowserWindow, WebContentsView } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-        width: 800,
-        height: 600,
+        width: 1200,
+        height: 800,
     })
 
-    win.loadFile('index.html');
+    const view1 = new WebContentsView()
+    win.contentView.addChildView(view1)
+    view1.webContents.loadURL('https://sunliner.de')
+    view1.setBounds({ x: 0, y: 0, width: 400, height: 400 })
 
-    shell.openExternal('https://kreando.ch');
-    shell.openExternal('https://sunliner.de');
-    shell.openExternal('https://das-radhaus.de');
-    shell.showItemInFolder('./test.md');
-    shell.beep();
-    // shell.trashItem('/Users/imanaboheydary/Desktop/projects/electron/ke/test.md');
+    const view2 = new WebContentsView()
+    win.contentView.addChildView(view2)
+    view2.webContents.loadURL('https://kreando.ch')
+    view2.setBounds({ x: 400, y: 0, width: 400, height: 400 })
+
+    const view3 = new WebContentsView()
+    win.contentView.addChildView(view3)
+    view3.webContents.loadURL('https://das-radhaus.de')
+    view3.setBounds({ x: 0, y: 400, width: 400, height: 400 })
 }
 
 app.whenReady().then(() => {
