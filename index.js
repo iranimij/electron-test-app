34a357899bfb1fb20ac8cdc97ec498ce207b9141
Iman Aboheydary <iman@customgento.com>
2024-11-29 11:06:21 +0100

    shell module

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, shell } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
@@ -8,8 +8,12 @@ const createWindow = () => {
 
     win.loadFile('index.html');
 
-    const child = new BrowserWindow({ parent: win });
-    child.loadURL('https://kreando.ch');
+    shell.openExternal('https://kreando.ch');
+    shell.openExternal('https://sunliner.de');
+    shell.openExternal('https://das-radhaus.de');
+    shell.showItemInFolder('./test.md');
+    shell.beep();
+    // shell.trashItem('/Users/imanaboheydary/Desktop/projects/electron/ke/test.md');
 }
 
 app.whenReady().then(() => {
