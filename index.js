26f2c0f970c1b4ed97299eb73c08ebb92c92e2c7
Iman Aboheydary <iman@customgento.com>
2024-11-29 11:42:33 +0100

    preload scripts

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-const { app, BrowserWindow, WebContentsView } = require('electron')
+const { app, BrowserWindow, WebContentsView } = require('electron');
+const path = require('node:path');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 1200,
         height: 800,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
     })
 
-    const view1 = new WebContentsView()
-    win.contentView.addChildView(view1)
-    view1.webContents.loadURL('https://sunliner.de')
-    view1.setBounds({ x: 0, y: 0, width: 400, height: 400 })
-
-    const view2 = new WebContentsView()
-    win.contentView.addChildView(view2)
-    view2.webContents.loadURL('https://kreando.ch')
-    view2.setBounds({ x: 400, y: 0, width: 400, height: 400 })
-
-    const view3 = new WebContentsView()
-    win.contentView.addChildView(view3)
-    view3.webContents.loadURL('https://das-radhaus.de')
-    view3.setBounds({ x: 0, y: 400, width: 400, height: 400 })
+    win.loadFile('index.html')
 }
 
 app.whenReady().then(() => {
